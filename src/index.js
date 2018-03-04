77083761cdf3bf08790120647f8c4f3bb440e120
DziLean <gestaltldi@yandex.ru>
2018-03-04 13:12:52 +0300

    1. Hot module replacement

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import _ from 'lodash';
-import printMe from './print.js';
+import './styles.css'
+import _ from 'lodash'
+import printMe from './print.js'
 
 function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
+    var element = document.createElement('div')
+    var btn = document.createElement('button')
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    element.appendChild(btn);
+    btn.innerHTML = 'Click me and check the console!'
+    btn.onclick = printMe
+    element.appendChild(btn)
 
-    return element;
+    return element
 }
 
-document.body.appendChild(component());
+/** within HMR procedure we should apply new updates manually */
+let element = component() // Store the element to re-render on print.js changes
+document.body.appendChild(element)
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        document.body.removeChild(element)
+        element = component() // Re-render the "component" to update the click handler
+        document.body.appendChild(element)
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
+
 
 module.exports = {
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        app: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output management'
-        })
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     }
-};
+}
