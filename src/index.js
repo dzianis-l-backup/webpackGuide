d33ddf911bc5a1312ae963e573394a69a625f92b
DziLean <gestaltldi@yandex.ru>
2018-03-10 04:27:09 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-//import Print from './print.js'
 import _ from 'lodash'
+import numRef from './ref.json'
 
-function component() {
-    var element = document.createElement('div')
-
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    //element.onclick = Print.bind(null, 'Hello webpack!')
-
-    return element
+export function numToWord(num) {
+    return _.reduce(numRef, (accum, ref) => {
+        return ref.num === num ? ref.word : accum
+    }, '')
 }
 
-document.body.appendChild(component())
+export function wordToNum(word) {
+    return _.reduce(numRef, (accum, ref) => {
+        return ref.word === word && word.toLowerCase() ? ref.num : accum
+    }, -1)
+}
