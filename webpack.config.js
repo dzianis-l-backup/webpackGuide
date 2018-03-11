06debdefcc70a6671dcc4b85523ff33fdb0047c4
DziLean <gestaltldi@yandex.ru>
2018-03-11 09:35:46 +0300

    shimming

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,27 @@
 const path = require('path')
 const webpack = require('webpack')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-/** code splitting - 2 entries*/
-/**
- * lodash is included in both of 2 entries
- * */
 
 module.exports = {
     entry: {
-        index: './src/index.js',
-        vendor: ['lodash']
+        polyfills: './src/polyfills.js',
+        index: './src/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: require.resolve('index.js'),
+                use: 'imports-loader?this=>window'
+            }
+        ]
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HTMLWebpackPlugin({
-            title: 'Caching'
-        }),
-        new webpack.HashedModuleIdsPlugin(),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'vendor'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'manifest'
-        }),
-    ],
-    output: {
-        filename: '[name].[chunkhash].js',
-        chunkFilename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+        new webpack.ProvidePlugin({
+            /** courtesy of _.join treeshaking occurs*/
+            join: ['lodash', 'join']
+        })
+    ]
 }
